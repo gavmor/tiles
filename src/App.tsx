faaea4b7e90a2445637a9ddee23673cf00c480b8
Gavin Morgan <morgan.gavin@gmail.com>
2023-03-01 16:13:30 -0800

    yellow and blue checkers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import {h} from "preact"
-import {  useEffect} from "preact/hooks"
+import { h } from "preact"
+import { useEffect } from "preact/hooks"
 import "./app.css"
-import {DomUtil, GridLayer, map, tileLayer} from 'leaflet'
+import { DomUtil, GridLayer, map, tileLayer } from 'leaflet'
 
 import quadSrc from './quad.png'
 function quadPNG() {
@@ -11,31 +11,44 @@ function quadPNG() {
 }
 
 const CanvasLayer = GridLayer.extend({
-  createTile: function(coord: {x:Number, y:Number}){
-      // create a <canvas> element for drawing
-      const tile = DomUtil.create('canvas', 'leaflet-tile');
-
-      // setup tile width and height according to the options
-      const size = this.getTileSize();
-      tile.width = size.x;
-      tile.height = size.y;
+  createTile: function (coords: { x: number, y: number }) {
+    var error;
     
-      const ctx = tile.getContext('2d');
-      ctx && ctx.drawImage(quadPNG(), 0, 0)
-      ctx && drawHouse(ctx);
+    const tile = DomUtil.create('canvas', 'leaflet-tile');
+    const {x: width, y: height} = this.getTileSize();
+    Object.assign(tile, {width, height})
 
-      // return the tile so it can be rendered on screen
-      return tile;
+    const ctx = tile.getContext('2d');
+    // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+    const quad = quadPNG()
+    
+    const sprite = {
+      blue: [0, 0],
+      yellow: [quad.width / 2, 0]
+    }
+    
+    
+    console.log(coords.x)
+    ctx && ctx.drawImage(...[quad, ...sprite[spriteFor(coords)], quad.width / 2, quad.height / 2, 0, 0, width, height])
+    ctx && drawHouse(ctx);
+    // done(error, tile);
+
+    return tile;
   }
 });
 
 
+function spriteFor(coords: { x: number; y: number }) {
+  return coords.y % 2 
+    ? coords.x % 2 ? "blue" : "yellow"
+    : coords.x % 2 ? "yellow" : "blue" 
+}
 
 export function App() {
-      useEffect(() => {
-      const myMap = map('map').setView([0, 0], 10);
-      new CanvasLayer().addTo(myMap)
-    }, []);
+  useEffect(() => {
+    const myMap = map('map').setView([0, 0], 10);
+    new CanvasLayer().addTo(myMap)
+  }, []);
 
   return <div id="map"></div>
 }
@@ -49,14 +62,10 @@ test("App", {
 function drawHouse(ctx: CanvasRenderingContext2D) {
   ctx.lineWidth = 10;
 
-  // Wall
-  ctx.strokeRect(75, 140, 150, 110);
-
-  // Door
-  ctx.fillRect(130, 190, 40, 60);
-
-  // Roof
-  ctx.beginPath();
+  ctx.strokeRect(75, 140, 150, 110); // wall
+  ctx.fillRect(130, 190, 40, 60); // Door
+  
+  ctx.beginPath(); // Roof
   ctx.moveTo(50, 140);
   ctx.lineTo(150, 60);
   ctx.lineTo(250, 140);
